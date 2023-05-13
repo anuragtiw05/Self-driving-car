0d70954d688125c4df2f4cd47f71b9bfe69cf0a2
anuragti05 <111039057+anuragti05@users.noreply.github.com>
2023-05-13 12:48:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-[10:23 am, 13/05/2023] Aditya Raj (ECE): import glob
+import glob
 import os
 import sys
 import random
@@ -55,41 +55,38 @@ EPISODES = 100
 
 DISCOUNT = 0.99
 epsilon = 1
-EPSILON_DECAY = 0.95 ## 0.9975 99975
+EPSILON_DECAY = 0.95 
 MIN_EPSILON = 0.001
 
 AGGREGATE_STATS_EVERY = 10
 
 
-# Own Tensorboard class
+
 class ModifiedTensorBoard(TensorBoard):
 
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    
     def _init_(self, **kwargs):
         super()._init_(**kwargs)
         self.step = 1
         self.writer = tf.summary.FileWriter(self.log_dir)
 
-    # Overriding this method to stop creating default log writer
+    
     def set_model(self, model):
         pass
 
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
+ 
     def on_epoch_end(self, epoch, logs=None):
         self.update_stats(**logs)
 
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
+   
     def on_batch_end(self, batch, logs=None):
         pass
 
-    # Overrided, so won't close writer
+    
     def on_train_end(self, _):
         pass
 
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
+  
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
 
@@ -147,7 +144,7 @@ class CarEnv:
 
     def process_img(self, image):
         i = np.array(image.raw_data)
-        #print(i.shape)
+        
         i2 = i.reshape((self.im_height, self.im_width, 4))
         i3 = i2[:, :, :3]
         if self.SHOW_CAM:
@@ -210,7 +207,7 @@ class DQNAgent:
         return model
 
     def update_replay_memory(self, transition):
-        # transition = (current_state, action, reward, new_state, done)
+       
         self.replay_memory.append(transition)
 
     def train(self):
@@ -280,76 +277,75 @@ class DQNAgent:
 
 if _name_ == '_main_':
     FPS = 60
-    # For stats
+  
     ep_rewards = [-200]
 
-    # For more repetitive results
+    
     random.seed(1)
     np.random.seed(1)
     tf.set_random_seed(1)
 
-    # Memory fraction, used mostly when trai8ning multiple agents
+    
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
     tf.compat.v1.keras.backend.set_session(tf.compat.v1.Session(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options)))
 
-    # Create models folder
+    
     if not os.path.isdir('models'):
         os.makedirs('models')
 
-    # Create agent and environment
+   
     agent = DQNAgent()
     env = CarEnv()
 
 
-    # Start training thread and wait for training to be initialized
+    
     trainer_thread = Thread(target=agent.train_in_loop, daemon=True)
     trainer_thread.start()
     while not agent.training_initialized:
         time.sleep(0.01)
 
-    # Initialize predictions - forst prediction takes longer as of initialization that has to be done
-    # It's better to do a first prediction then before we start iterating over episode steps
+    
     agent.get_qs(np.ones((env.im_height, env.im_width, 3)))
 
-    # Iterate over episodes
+    
     for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
-        #try:
+        
 
             env.collision_hist = []
 
-            # Update tensorboard step every episode
+           
             agent.tensorboard.step = episode
 
-            # Restarting episode - reset episode reward and step number
+            
             episode_reward = 0
             step = 1
 
-            # Reset environment and get initial state
+         
             current_state = env.reset()
 
-            # Reset flag and start iterating until episode ends
+            
             done = False
             episode_start = time.time()
 
-            # Play for given number of seconds only
+           
             while True:
 
-                # This part stays mostly the same, the change is to query a model for Q values
+          
                 if np.random.random() > epsilon:
-                    # Get action from Q table
+                   
                     action = np.argmax(agent.get_qs(current_state))
                 else:
-                    # Get random action
+                   
                     action = np.random.randint(0, 3)
-                    # This takes no time, so we add a delay matching 60 FPS (prediction above takes longer)
+                    
                     time.sleep(1/FPS)
 
                 new_state, reward, done, _ = env.step(action)
 
-                # Transform new continous state to new discrete state and count reward
+               
                 episode_reward += reward
 
-                # Every step we update replay memory
+              
                 agent.update_replay_memory((current_state, action, reward, new_state, done))
 
                 current_state = new_state
@@ -358,11 +354,11 @@ if _name_ == '_main_':
                 if done:
                     break
 
-            # End of episode - destroy agents
+            
             for actor in env.actor_list:
                 actor.destroy()
 
-            # Append episode reward to a list and log stats (every given number of episodes)
+           
             ep_rewards.append(episode_reward)
             if not episode % AGGREGATE_STATS_EVERY or episode == 1:
                 average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
@@ -370,17 +366,17 @@ if _name_ == '_main_':
                 max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
                 agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
-                # Save model, but only when min reward is greater or equal a set value
+                
                 if min_reward >= MIN_REWARD:
                     agent.model.save(f'models/{MODEL_NAME}_{max_reward:>7.2f}max_{average_reward:>7.2f}avg{min_reward:>7.2f}min_{int(time.time())}.model')
 
-            # Decay epsilon
+     
             if epsilon > MIN_EPSILON:
                 epsilon *= EPSILON_DECAY
                 epsilon = max(MIN_EPSILON, epsilon)
 
 
-    # Set termination flag for training thread and wait for it to finish
+   
     agent.terminate = True
     trainer_thread.join()
     agent.model.save(f'models/{MODEL_NAME}_{max_reward:>7.2f}max_{average_reward:>7.2f}avg{min_reward:>7.2f}min_{int(time.time())}.model')
@@ -434,41 +430,38 @@ EPISODES = 100
 
 DISCOUNT = 0.99
 epsilon = 1
-EPSILON_DECAY = 0.95 ## 0.9975 99975
+EPSILON_DECAY = 0.95 
 MIN_EPSILON = 0.001
 
 AGGREGATE_STATS_EVERY = 10
 
 
-# Own Tensorboard class
+
 class ModifiedTensorBoard(TensorBoard):
 
-    # Overriding init to set initial step and writer (we want one log file for all .fit() calls)
+    
     def _init_(self, **kwargs):
         super()._init_(**kwargs)
         self.step = 1
         self.writer = tf.summary.FileWriter(self.log_dir)
 
-    # Overriding this method to stop creating default log writer
+    
     def set_model(self, model):
         pass
 
-    # Overrided, saves logs with our step number
-    # (otherwise every .fit() will start writing from 0th step)
+   
     def on_epoch_end(self, epoch, logs=None):
         self.update_stats(**logs)
 
-    # Overrided
-    # We train for one batch only, no need to save anything at epoch end
+   
     def on_batch_end(self, batch, logs=None):
         pass
 
-    # Overrided, so won't close writer
+ 
     def on_train_end(self, _):
         pass
 
-    # Custom method for saving own metrics
-    # Creates writer, writes custom metrics and closes writer
+   
     def update_stats(self, **stats):
         self._write_logs(stats, self.step)
 
@@ -589,7 +582,7 @@ class DQNAgent:
         return model
 
     def update_replay_memory(self, transition):
-        # transition = (current_state, action, reward, new_state, done)
+      
         self.replay_memory.append(transition)
 
     def train(self):
@@ -659,76 +652,75 @@ class DQNAgent:
 
 if _name_ == '_main_':
     FPS = 60
-    # For stats
+   
     ep_rewards = [-200]
 
-    # For more repetitive results
+   
     random.seed(1)
     np.random.seed(1)
     tf.set_random_seed(1)
 
-    # Memory fraction, used mostly when trai8ning multiple agents
+    
     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=MEMORY_FRACTION)
     backend.set_session(tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)))
 
-    # Create models folder
+   
     if not os.path.isdir('models'):
         os.makedirs('models')
 
-    # Create agent and environment
+   
     agent = DQNAgent()
     env = CarEnv()
 
 
-    # Start training thread and wait for training to be initialized
+   
     trainer_thread = Thread(target=agent.train_in_loop, daemon=True)
     trainer_thread.start()
     while not agent.training_initialized:
         time.sleep(0.01)
 
-    # Initialize predictions - forst prediction takes longer as of initialization that has to be done
-    # It's better to do a first prediction then before we start iterating over episode steps
+   
     agent.get_qs(np.ones((env.im_height, env.im_width, 3)))
 
-    # Iterate over episodes
+   
     for episode in tqdm(range(1, EPISODES + 1), ascii=True, unit='episodes'):
-        #try:
+       
 
             env.collision_hist = []
 
-            # Update tensorboard step every episode
+            
             agent.tensorboard.step = episode
 
-            # Restarting episode - reset episode reward and step number
+           
             episode_reward = 0
             step = 1
 
-            # Reset environment and get initial state
+           
             current_state = env.reset()
 
-            # Reset flag and start iterating until episode ends
+            
             done = False
             episode_start = time.time()
 
-            # Play for given number of seconds only
+           
             while True:
 
-                # This part stays mostly the same, the change is to query a model for Q values
+              
                 if np.random.random() > epsilon:
-                    # Get action from Q table
+                  
                     action = np.argmax(agent.get_qs(current_state))
                 else:
-                    # Get random action
+                 
                     action = np.random.randint(0, 3)
-                    # This takes no time, so we add a delay matching 60 FPS (prediction above takes longer)
+                  
                     time.sleep(1/FPS)
 
                 new_state, reward, done, _ = env.step(action)
 
-                # Transform new continous state to new discrete state and count reward
+             
                 episode_reward += reward
 
-                # Every step we update replay memory
+                
                 agent.update_replay_memory((current_state, action, reward, new_state, done))
 
                 current_state = new_state
@@ -737,11 +729,11 @@ if _name_ == '_main_':
                 if done:
                     break
 
-            # End of episode - destroy agents
+            
             for actor in env.actor_list:
                 actor.destroy()
 
-            # Append episode reward to a list and log stats (every given number of episodes)
+          
             ep_rewards.append(episode_reward)
             if not episode % AGGREGATE_STATS_EVERY or episode == 1:
                 average_reward = sum(ep_rewards[-AGGREGATE_STATS_EVERY:])/len(ep_rewards[-AGGREGATE_STATS_EVERY:])
@@ -749,17 +741,16 @@ if _name_ == '_main_':
                 max_reward = max(ep_rewards[-AGGREGATE_STATS_EVERY:])
                 agent.tensorboard.update_stats(reward_avg=average_reward, reward_min=min_reward, reward_max=max_reward, epsilon=epsilon)
 
-                # Save model, but only when min reward is greater or equal a set value
                 if min_reward >= MIN_REWARD:
                     agent.model.save(f'models/{MODEL_NAME}_{max_reward:>7.2f}max_{average_reward:>7.2f}avg{min_reward:>7.2f}min_{int(time.time())}.model')
 
-            # Decay epsilon
+        
             if epsilon > MIN_EPSILON:
                 epsilon *= EPSILON_DECAY
                 epsilon = max(MIN_EPSILON, epsilon)
 
 
-    # Set termination flag for training thread and wait for it to finish
+    
     agent.terminate = True
     trainer_thread.join()
     agent.model.save(f'models/{MODEL_NAME}_{max_reward:>7.2f}max_{average_reward:>7.2f}avg{min_reward:>7.2f}min_{int(time.time())}.model')
